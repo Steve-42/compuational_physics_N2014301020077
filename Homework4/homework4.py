5abd1321063546022a2437f60669f30613ac2089
Steve-42 <houguoyi@live.com>
2016-10-09 22:00:35 +0800

    Update homework4.py

--- Homework4/homework4.py
+++ Homework4/homework4.py
@@ -15,28 +15,29 @@ class nuclei_decay:
         print 'Time constant ->' , time_constant
         print 'time step -> ' , time_step
         print 'total time -> ' , time_of_duration
-    def calculate(self):
+    def calculate(self): #单独模拟
         for i in range(self.nstep):
             tmp = self.n[i] - self.n[i] / self.tau * self.dt
             self.n.append(tmp)
             self.t.append(self.t[i] + self.dt)
-    def step_begin(self): #计算单步衰变原子核数变化量
-        self.dn = - self.n[-1] / self.tau  * self.dt
-    def step_end(self, delta_N = 0): #存储单步衰变数据
-        self.n.append(self.n[-1] + delta_N)
-        self.t.append(self.t[-1] + self.dt)
-    def show_results(self):
+    def show_results(self): #输出图表
         pl.plot(self.t, self.n)
         pl.xlabel('time ($s$)')
         pl.ylabel('Number of Nuclei' + self.name)
         pl.show()
-    def store_results(self):
+    def store_results(self): #存储数据
         filename = 'nuclei_'+self.name+'_decay_data.txt'
         file = open(filename, 'w')
         for i in range(len(self.t)):
             print >> file, ' %5.2f\t %10.6f' % (self.t[i],self.n[i])
         file.close()
-# 含缺省的输入函数
+    def step_begin(self): #计算单步衰变原子核数变化量
+        self.dn = - self.n[-1] / self.tau  * self.dt
+    def step_end(self, delta_N = 0): #存储单步衰变数据
+        self.n.append(self.n[-1] + delta_N)
+        self.t.append(self.t[-1] + self.dt)
+
+#含缺省的输入函数
 def get(msg,default = 0):
     r = raw_input(msg)
     if r == '':
@@ -67,4 +68,6 @@ title('Transformation between A and B')
 xlabel('time($s$)')
 ylabel('Number of Nuclei')
 legend([p1,p2], ['Nuclei A', 'Nuclei B'])
+savefig('figure.png')
 show()
+
